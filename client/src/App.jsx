cdb62acdbac7b2bf2d25daf779278fe8fa943446
Utkarsh <utkarsh_fw11_075@masai.school>
2021-12-15 11:32:57 +0530

    Just Checking Around

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,39 @@ function App() {
 
   return (
     <div className="App">
-     
+
+      <div className='container'>
+
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img className="d-block w-100" src="https://images.unsplash.com/photo-1528468107336-9b04ba537b74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="First slide" style={{maxWidth:"500px"}}/>
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src="https://images.unsplash.com/photo-1544582024-63fb0749d201?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwcm9maWxlLXBhZ2V8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="Second slide" style={{maxWidth:"500px"}}/>
+            </div>
+            <div className="carousel-item">
+              <img className="d-block w-100" src="https://images.unsplash.com/photo-1539459097723-a4a0464bcf7b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Third slide" style={{maxWidth:"500px"}} />
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+
+      </div>
     </div>
+
   );
 }
 
