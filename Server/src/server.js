22cf76a62f1c5b3afba6fe486a934aa7a800705f
vishalpatil12014017 <87421912+vishalpatil12014017@users.noreply.github.com>
2021-12-17 23:35:33 +0530

    Utils Added

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,10 +1,75 @@
 const express = require("express")
 const connect = require("./config/db")
-
+//rewire for signup
+const { parse } = require("path");
+const crypto = require('crypto');
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
+//
 const app = express()
 
 const port = process.env.PORT || 3535;
+///code for siginin
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const smsKey = process.env.SMS_SECRET_KEY;
+const client = require('twilio')(accountSid,authToken);
 
+//const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN;
+
+const cors = require('cors');
+
+//let refreshTokens = [];
+app.use(cookieParser());
+app.use(cors({origin:'http://localhost:3000',credentials:true}))
+/******************* Send OTP **************/
+
+app.post('/sendOTP',(req,res)=>{
+    const phone = req.body.phone;
+    const otp = Math.floor(100000+ Math.random()*900000);
+    const ttl = 2*60*1000;
+    const expires = Date.now() +ttl;
+    const data = `${phone}.${otp}.${expires}`
+    const hash = crypto.createHmac('sha256',smsKey).update(data).digest("hex");
+    const fullhash = `${hash}.${expires}`;
+
+    client.messages.create({
+        body:`Your One Time Password (OTP) for Sign-In is ${otp}`,
+        from:+12348039855,
+        to:phone
+    }).then((messages)=>console.log(messages)).catch((err)=>console.error(err))
+
+     res.status(200).send({phone,hash:fullhash,otp})
+})
+
+/************************* Verify router ******************/
+
+app.post("/verifyOTP", (req,res)=>{
+    const phone = req.body.phone;
+    const hash  = req.body.hash;
+    
+    const otp = req.body.otp;
+
+    let [hashValue,expires] = hash.split(".");
+
+    let now  = Date.now()
+    if(now>parseInt(expires)){
+        return res.status(504).send({msg:"Timeout please try again!"})
+    } 
+
+    const data = `${phone}.${otp}.${expires}`;
+    const newCalculatedHash = crypto.createHmac('sha256',smsKey).update(data).digest('hex')
+    
+    if(newCalculatedHash === hashValue){
+       
+
+        res.status(202).send({msg:`Device Verified`})
+
+    }else{
+        return res.status(400).send({verification:false,msg:"Incorrect OTP"})
+    }
+})
 app.use(express.json())
 //controllers
 const BestController = require("./controllers/BestController")
@@ -26,9 +91,7 @@ app.use("/Favhindi", FavhindiController )
 app.use("/showlove", showloveController )
 
 app.listen(port, async () => {
-
     await connect();
-
     console.log(`listening to ${port}`)
     
 })
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,48 @@
 
 import './App.css';
+import Carosule from './Component/Pages/Home/Carosule';
+import Navbar from './Component/Pages/Navbar/Navbar';
+import Slider from './Component/Pages/Home/Slider';
+import Showlove from './Component/Pages/Home/Showlove';
+import Showloveall from './Component/Pages/Home/Showloveall';
+import Newly from './Component/Pages/Home/Newly'
+import { Switch, Route } from 'react-router-dom';
+import Watch from './Component/Pages/Watch/Watch';
+import Free from './Component/Pages/Home/Free';
+import Best from './Component/Pages/Home/Best';
+import Hindi from './Component/Pages/Home/Hindi'
+import Tamil from './Component/Pages/Home/Tamil'
+import Famous from './Component/Pages/Home/Famous';
+import Sliderwatch from './Component/Pages/Watch/Sliderwatch';
+import { Footer } from './Component/Pages/Footer/Footer';
 function App() {
-
   return (
-    <div className="App">
-     
-    </div>
+    <div className="App fluid-container pb-0">
+      <Navbar></Navbar>
+      <Switch>
+        <Route path="/" exact>
+          <Carosule />
+          <Slider></Slider>
+          <Showlove />
+          <Newly />
+          <Free/>
+          <Best/>
+          <Hindi/>
+          <Tamil/>
+          <Famous/>
+        </Route>
+        <Route path="/showall" >
+          <Showloveall />
+        </Route>
+        <Route path="/watch" >
+          <Watch />
+        </Route>
+        <Route path="/swatch" >
+          <Sliderwatch />
+        </Route>
+      </Switch>
+      <Footer/>
+    </div >
   );
 }
 
